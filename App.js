18485ec36bfe3c7c655951220a8288f8ff4f67a2
Murtaza Ahmed <murtazamalik1319@gmail.com>
2025-08-18 00:07:23 +0530

    Initial commit

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const App = () => {
+  return (
+    <View>
+      <Text> Welcome Go Build</Text>
+    </View>
+  )
+}
+
+export default App;
