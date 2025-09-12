435de545b1fcaef5f83c61095fc25c9a22cf7ee7
092vk <vk092kumar@gmail.com>
2025-09-12 08:50:04 +0530

    added config for supabase

---
+++ Src/integrations/supabase/client.js
@@ -0,0 +1,18 @@
+// setting up the supabase client to be used in other files
+
+import 'react-native-url-polyfill/auto';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { createClient } from '@supabase/supabase-js';
+import { SUPABASE_URL, SUPABASE_PUBLISHABLE_KEY } from '@env';
+
+const supabaseUrl = SUPABASE_URL;
+const supabaseAnonKey = SUPABASE_PUBLISHABLE_KEY;
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+    auth: {
+        storage: AsyncStorage,
+        autoRefreshToken: true,
+        persistSession: true,
+        detectSessionInUrl: false,
+    },
+});
